aea927fc7774b2277bc21b4ab8cd495f4ca758b9
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-05-25 09:03:54 +0530

    general navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Homepage from "./Homepage";
+import AboutMe from "./AboutMe";
 
 function App() {
-  return (
-    <h1>Hello</h1>
-  );
+    return (
+        <div className="App">
+            <nav className="nav">
+                <a href="#" className="nav-item">Homepage</a>
+                <a href="#" className="nav-item">About Me</a>
+            </nav>
+            <Homepage/>
+            <AboutMe/>
+        </div>
+    );
 }
 
 export default App;
