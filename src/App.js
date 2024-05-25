50479a9a98a446f0d304169658a6286bb7f91ad8
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-05-25 22:10:35 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Homepage from "./Homepage";
 import AboutMe from "./AboutMe";
 import {Link, Route, Routes} from "react-router-dom";
+import Contact from "./Contact";
 
 function App() {
     return (
@@ -9,10 +10,12 @@ function App() {
             <nav className="nav">
                 <Link to="/" className="nav-item">Homepage</Link>
                 <Link to="/about-me" className="nav-item">About Me</Link>
+                <Link to="/contact" className="nav-item">Contact</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Homepage/>}></Route>
                 <Route path="/about-me" element={<AboutMe/>}></Route>
+                <Route path="/contact" element={<Contact/>}></Route>
             </Routes>
         </div>
     );
