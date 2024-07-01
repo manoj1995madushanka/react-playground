1b402a5966d36025c9f796bc0fd5ea45093889c0
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-07-01 18:00:42 +0530

    API calls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <h1>Hello</h1>
+  const [user, setUser] = useState([]);
+
+  const fetchData = () => {
+    fetch( "https://randomuser.me/api/?results=1")
+        .then((response) => response.json())
+        .then(data => setUser(data))
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return Object.keys(user).length > 0 ? (
+      <div>
+        <h1>Customer data</h1>
+        <h2>Name:{user.results[0].name.first}</h2>
+        <img src={user.results[0].picture.large} alt=""/>
+      </div>
+  ) : (
+      <h1>Data pending...</h1>
   );
 }
 
