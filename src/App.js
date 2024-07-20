c2b9e1014f659c690fde232fda2181f5d77356b4
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-07-20 20:17:31 +0530

    containtment vs specialization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
 
+const Button = ({children,backgroundColor}) => {
+  return <button style={{backgroundColor}}>{children}</button>
+}
+
+const Alert = ({children})=>{
+  return(
+      <>
+        <div className="Overlay"></div>
+        <div className="Alert">{children}</div>
+      </>
+  )
+}
+
+const DeleteButton = () => {
+  return <Button backgroundColor="red">Delete</Button>
+}
+
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="App">
+      <header>Little Lemon Restaurent</header>
+      <Alert>
+        <h4>Delete Account</h4>
+        <p>
+          Are you sure?
+        </p>
+        <DeleteButton/>
+      </Alert>
+    </div>
   );
 }
 
